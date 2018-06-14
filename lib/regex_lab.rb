bd96464b671056bf8f86b0505bebd288ac6c8fb0
Emily Wessel <ettl02@gmail.com>
2018-06-14 21:31:03 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -12,10 +12,10 @@ text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-if text.match(/\A[A-Z]. +[.,?!]\z/)
-return true
+if text.match(/\A[A-Z].+[.,?!]\z/)
+ true
 else
-  return false
+  false
 end
 end
 
