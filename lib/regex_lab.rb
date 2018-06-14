a7b15378997dfcdf5e9471031b44d59654ad0c54
Emily Wessel <ettl02@gmail.com>
2018-06-14 21:29:52 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -12,8 +12,11 @@ text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-!!text.match(/\A[A-Z]. +[.,?!]\z/)
-end
+if text.match(/\A[A-Z]. +[.,?!]\z/)
+return true
+else 
+  return false 
+end 
 
 def valid_phone_number?(phone)
 
